97683b978a152acbf6fde19799ea7eebad05d1c9
Juan Carlos <93095797+JuanCarlosFR26@users.noreply.github.com>
2021-12-11 18:54:23 +0100

    Add files via upload

---
+++ Ejercicios Tema 1/src/com/example/MainVariables.java
@@ -0,0 +1,50 @@
+package com.example;
+
+public class MainVariables {
+
+    //Metiendo comentarios
+
+    /**
+     * Otro tipo de comentario
+     */
+    public static void main(String[] args) {
+
+        //Tipo de variables:
+
+        // 1. Numéricos:
+        int numero1 = 44;
+        long numeroLong = 458392L;
+        double numeroDec = 31.12d;
+        float numeroFl = 3.5f;
+        byte numeroByte = 2;
+        short numeroShort = 10;
+
+        System.out.println(numero1);
+        System.out.println(numeroLong);
+        System.out.println(numeroDec);
+        System.out.println(numeroFl);
+        System.out.println(numeroByte);
+        System.out.println(numeroShort);
+
+        // 2. Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println(verdadero);
+        System.out.println(falso);
+
+
+        // 3. Texto:
+        String nombre = "Juan";
+        char caracter = 'a';
+
+        System.out.println(nombre);
+        System.out.println(caracter);
+
+
+
+
+
+
+    }
+}
